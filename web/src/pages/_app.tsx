8d4cf0b680895b3a292ab8a98e3fa220a7bd4bb4
Kristiane Westgård <kristiane.westgard@blank.no>
2021-02-05 10:21:15 +0100

    env stuff

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,28 +1,31 @@
-import { AppProps } from 'next/app';
-import '@/styles/global.css';
-import Head from 'next/head';
-
+import { AppProps } from "next/app";
+import "@/styles/global.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const gaId = process.env.GOOGLE_ANALYTICS_ID;
   return (
     <>
       <Head>
-        {gaId &&
+        {gaId && (
           <>
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
-            <script dangerouslySetInnerHTML={{
-              __html: ` window.dataLayer = window.dataLayer || [];
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: ` window.dataLayer = window.dataLayer || [];
               function gtag(){window.dataLayer.push(arguments)}
               gtag('js', new Date());
-              gtag('config', '${gaId}');`
-            }}
+              gtag('config', '${gaId}');`,
+              }}
             />
           </>
-        }
+        )}
         <title>Tromsø strikkefestival</title>
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
